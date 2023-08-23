c2b60003da0928aeb64d151587980ce37235c289
Slobodan Klentikov <slobodan_kletnikov@hotmail.com>
2023-08-23 18:35:00 +0200

    Add watch app

---
+++ ios/TeamScore Watch App/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  TeamScore Watch App
+//
+//  Created by Slobodan Kletnikov on 23.8.23.
+//
+
+import SwiftUI
+
+struct TeamScore: View {
+    var color: Color
+    @State private var score: Int = 0
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius:11)
+                .fill(color)
+            Text(String(score))
+        }.onTapGesture { location in
+            if (location.x < 100) {
+                score-=1;
+            } else {
+                score+=1;
+            }
+          }
+    }
+}
+
+struct ContentView: View {
+    @State private var selection = 1
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            Button("RESET") {
+                print("Button tapped!")
+            }.tag(0)
+            VStack {
+                TeamScore(color: .green)
+                TeamScore(color: .red)
+            }.tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/TeamScore Watch App/TeamScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamScoreApp.swift
+//  TeamScore Watch App
+//
+//  Created by Slobodan Kletnikov on 23.8.23.
+//
+
+import SwiftUI
+
+@main
+struct TeamScore_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
