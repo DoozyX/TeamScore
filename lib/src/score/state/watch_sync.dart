cc361730e86c5041115e04a64ebc04957ecac863
Slobodan Klentikov <slobodan_kletnikov@hotmail.com>
2023-08-25 08:12:41 +0200

    Fix lints

--- lib/src/score/state/watch_sync.dart
+++ lib/src/score/state/watch_sync.dart
@@ -26,14 +26,13 @@ void watchSync(WatchSyncRef ref) {
   final hostApi = TeamScoreHostApi();
 
   ref.listen(scoreProvider, (previous, next) {
-    print('[FLUTTER] score changed ${next.team1Score} - ${next.team2Score}');
-    print('changed $lastSynced $next  ${lastSynced == next}');
-    ;
+    // print('[FLUTTER] score changed ${next.team1Score} - ${next.team2Score}');
+    // print('changed $lastSynced $next  ${lastSynced == next}');
     if (lastSynced == next) {
-      print('[FLUTTER] already synced');
+      // print('[FLUTTER] already synced');
       return;
     }
-    print('[FLUTTER] send score: ${next.team1Score} - ${next.team2Score}');
+    // print('[FLUTTER] send score: ${next.team1Score} - ${next.team2Score}');
     hostApi.sendScore(
       MessageData(
         team1Score: next.team1Score,
@@ -46,10 +45,10 @@ void watchSync(WatchSyncRef ref) {
   final scoreNotifier = ref.read(scoreProvider.notifier);
   TeamScoreFlutterApi.setup(
     TeamScoreFlutterApiHandler((score) {
-      print(
-          '[FLUTTER] receive score: ${score.team1Score} - ${score.team2Score}');
+      // print(
+      //     '[FLUTTER] receive score: ${score.team1Score} - ${score.team2Score}');
       if (lastSynced == score) {
-        print('[FLUTTER] already synced');
+        // print('[FLUTTER] already synced');
         return;
       }
       lastSynced = score;
